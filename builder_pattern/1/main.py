286a94ea7edb9bcc530bc400ff3b2826076bff1e
kenntoot <lefthandmastery@gmail.com>
2023-01-08 19:58:38 +0800

    Added docstring and source for builder_pattern

--- builder_pattern/1/main.py
+++ builder_pattern/1/main.py
@@ -26,9 +26,15 @@ class Builder(ABC):
         pass
 
 class ConcreteBuilder1(Builder):
-    
+    """The Concrete builder classes follow the Builder interface and
+    provide specific implementations of the building steps. Your program may have
+    several variations of Builders, implemented differently.
+    """
 
     def __init__(self) -> None:
+        """A fresh builder instance should contain a blank product object, which is
+        used in the further assemly.
+        """
         self.reset()
 
     def reset(self) -> None:
@@ -36,6 +42,19 @@ class ConcreteBuilder1(Builder):
 
     @property
     def product(self) -> Product1:
+        """Concrete Builders are supposed to provide their own methods for retreiving
+        results. That's because various types of builders may create entirely different
+        products that don't follow the same interface. Therefore, such methods cannot
+        be declared in the base Builder interface (at least in a statically typed programming
+        language).
+
+        Usually, after returning the end result to the client, a builder instance is expected
+        to be ready to start producing another product. That's why it's a usual practice to call
+        the reset method at the end of the `getProduct` method body. However, this behavior is
+        not mandatory, and you can make your builders wait for an explicit reset call from the
+        client code before disposing of the previous result.
+        
+        """
 
         product = self._product
         self.reset()
@@ -52,6 +71,12 @@ class ConcreteBuilder1(Builder):
 
 
 class Product1():
+    """It makes sense to use the Builder pattern only when your products are quite complex and
+    require extensive configuration
+
+    Unlike in other creational patterns, different concrete builders can produce unrelated products.
+    In other words, results of various builders may not always follow the same interface.
+    """
 
     def __init__(self) -> None:
         self.parts = []
@@ -64,7 +89,9 @@ class Product1():
 
 
 class Director:
-
+    """The director works with any builder instance that client code passes to it. This way, the 
+    client code may alter the final type of the newly assembled product.
+    """
     def __init__(self) -> None:
         self._builder = None
 
@@ -76,6 +103,9 @@ class Director:
     def builder(self, builder: Builder) -> None:
 
         self._builder = builder
+
+    """The director can construct several product variations using the same building stemps.
+    """
     
 
     def build_minimal_viable_product(self) -> None:
