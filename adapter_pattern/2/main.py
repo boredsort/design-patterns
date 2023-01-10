19ac891774c04d1d9b19a9e472745abcf9093e27
kenntoot <lefthandmastery@gmail.com>
2023-01-10 21:57:43 +0800

    Updated adapter pattern

--- adapter_pattern/2/main.py
+++ adapter_pattern/2/main.py
@@ -1,12 +1,15 @@
 import json
+from bs4 import BeautifulSoup
 
+from xml_adapter import XMLConfig
 from experiment import Experiment
 
 def main() -> None:
     with open('config.xml', encoding='utf-8') as file:
         config = file.read()
-
-    experiment = Experiment(config)
+    bs = BeautifulSoup(config, 'lxml')
+    adapter = XMLConfig((bs))
+    experiment = Experiment(adapter)
     experiment.run()
 
 if __name__ == "__main__":
